1ebff1b00e746d523069683b96a3e37a700c65a5
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-10-23 22:11:11 +0600

    work on OnClick for Want to cook btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "./Components/Banner/Banner";
 import Header from "./Components/Header/Header";
 import OurReciepe from "./Components/Our_Reciepe/OurReciepe";
@@ -5,6 +6,21 @@ import Recipes from "./Components/Recipes.jsx/Recipes";
 import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
+  const [recipeClick, setRecipeClick] = useState([]);
+
+  const addRecipeOnClick = recipes => {
+    const isExist = recipeClick.find(previousRecipe => previousRecipe.recipe_id === recipes.recipe_id)
+    if (!isExist) {
+      setRecipeClick([...recipeClick, recipes])
+    }
+    else{
+      alert('Recipe already exists in the queue.')
+    }
+  }
+
+  console.log(recipeClick)
+
+
   return (
     <>
       <section className='max-w-screen-2xl mx-auto'>
@@ -21,10 +37,12 @@ function App() {
         {/* recipe card section */}
         <section className="flex flex-col md:flex-row gap-6 mb-24 mt-24">
            {/* card section */}
-            <Recipes></Recipes>
+            <Recipes addRecipeOnClick={addRecipeOnClick}
+            ></Recipes>
 
            {/* side bar */}
-           <Sidebar></Sidebar>
+           <Sidebar recipeClick={recipeClick}
+           ></Sidebar>
 
         </section>
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,18 @@
+import PropTypes from "prop-types";
 
-const Sidebar = () => {
+const Sidebar = ({recipeClick}) => {
     
 
     return (
         <div className="md:w-1/3">
-            <h1>sidebar:</h1>
+            <h1>Want To Cook: {recipeClick.length}</h1>
         </div>
     );
 };
+
+
+Sidebar.propTypes = {
+    recipeClick: PropTypes.array
+}
 
 export default Sidebar;
